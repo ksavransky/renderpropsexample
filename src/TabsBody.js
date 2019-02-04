9d7edad2fe49b165cfc1f4dc9a41921919287003
Konstantin Savransky <ksavransky@gmail.com>
2019-02-03 20:18:35 -0800

    styling

--- src/TabsBody.js
+++ src/TabsBody.js
@@ -4,8 +4,8 @@ import './App.css';
 class TabsBody extends Component {
   render() {
     return (
-      <div className="tabs-body">
-        {this.props.content || 'Empty'}
+      <div className="tabs-body-yellow">
+        {this.props.content}
       </div>
     );
   }
